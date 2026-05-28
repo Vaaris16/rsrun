a0591b5cc340cd09fc802f07086d1f314bc653d3
Vaaris16 <vaarischitkara@Vaaris16s-MacBook-Pro.local>
2026-05-28 14:04:16 +0530

    made a function to watch a dir for changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Result, Watcher};
+use std::path::Path;
+use std::sync::mpsc::channel;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = watch_directory("./") {
+        print!("Error: {}", e);
+    }
+}
+
+fn watch_directory(path: &str) -> Result<()> {
+    let (tx, rx) = channel::<Result<Event>>();
+
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+
+    watcher.watch(Path::new(path), RecursiveMode::Recursive)?;
+
+    println!("Watching: {path}");
+
+    for res in rx {
+        println!("change detected")
+    }
+
+    Ok(())
 }
