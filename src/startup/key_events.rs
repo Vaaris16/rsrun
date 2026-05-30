82ac6619a714eee08cba764ad9228d33ec50b4c2
Vaaris16 <vaarischitkara@Vaaris16s-MacBook-Pro.local>
2026-05-30 10:55:27 +0530

    added colors

--- src/startup/key_events.rs
+++ src/startup/key_events.rs
@@ -6,16 +6,17 @@ pub fn key_press() {
     for line in stdin.lock().lines() {
         match line.unwrap().trim().to_string().as_str() {
             "r" => {
-                println!("Manual Reload...");
+                println!("\x1b[1;36mManual Reload...\x1b[0m");
                 if let Err(e) = on_change() {
                     eprintln!("Error: {e}");
                 }
             }
             "q" => std::process::exit(0),
             "h" => {
-                println!("  r + enter: manual reload");
-                println!("  q + enter: quit");
-                println!("  h + enter: help");
+                println!("\x1b[1;33mKeybinds:\x1b[0m\r");
+                println!("  \x1b[1mр + enter:\x1b[0m manual reload");
+                println!("  \x1b[1mq + enter:\x1b[0m quit");
+                println!("  \x1b[1mh + enter:\x1b[0m help");
             }
             _ => {}
         }
--- src/startup/startup_ui.rs
+++ src/startup/startup_ui.rs
@@ -3,10 +3,13 @@ use figlet_rs::FIGlet;
 pub fn startup_ui(path: &str) {
     let slant_font = FIGlet::slant().unwrap();
 
-    println!("Watching: {}\r", path);
-    println!("{}\r", slant_font.convert("RSRUN").unwrap());
-    println!("Keybinds:\r");
-    println!("  r + enter: manual reload");
-    println!("  q + enter: quit");
-    println!("  h + enter: help");
+    println!("\x1b[1;36mWatching:\x1b[0m {}\r", path);
+    println!(
+        "\x1b[1;32m{}\x1b[0m\r",
+        slant_font.convert("RSRUN").unwrap().to_string()
+    );
+    println!("\x1b[1;33mKeybinds:\x1b[0m\r");
+    println!("  \x1b[1mр + enter:\x1b[0m manual reload");
+    println!("  \x1b[1mq + enter:\x1b[0m quit");
+    println!("  \x1b[1mh + enter:\x1b[0m help");
 }
